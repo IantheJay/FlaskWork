48d6c7a5b87a1e609c1d05d6e301301cf0baf360
IantheJay <ianjosephbrown@gmail.com>
2017-04-11 20:25:06 -0500

    Initial flask commit

---
+++ flaskDemo/varRules.py
@@ -0,0 +1,10 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/user/<username>')
+def show_user_profile(username):
+   return 'User %s' % username
+
+@app.route('/post/<int:post_id>')
+def show_post(post_id):
+   return 'Post %d' % post_id
